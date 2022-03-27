f6acfa437b9503d1e8d677bef68fc6054cf7413f
geoshavel <geoshavel@gmail.com>
2022-03-27 12:43:42 +0300

    init

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {
+	Routes,
+	Route,
+	useLocation,
+} from 'react-router-dom';
+
+// import * as PAGES from './pages';
+
+const App = () => {
+	return (
+		<div className="wrapper">
+			f
+		</div>
+	);
+};
+
+export default App;
---
+++ src/index.tsx
@@ -0,0 +1,25 @@
+/* eslint-disable import/no-unresolved */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+// eslint-disable-next-line import/extensions
+// import { AuthState } from 'context/auth';
+import App from './App';
+// import './utils/i18n';
+// eslint-disable-next-line import/extensions
+import reportWebVitals from './reportWebVitals';
+import './styles/main_global.scss';
+
+ReactDOM.render(
+	<React.StrictMode>
+		<Router>
+			<App />
+		</Router>
+	</React.StrictMode>,
+	document.getElementById('root'),
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
